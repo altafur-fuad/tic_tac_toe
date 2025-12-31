0fb6bb985d404f30c0322098e3db3a72e25e94b1
Altafur Rahman Fuad <altaf1fuad2@gmail.com>
2025-12-31 11:38:01 +0600

    tic tac toe 2

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,32 @@ import 'widget/box_state_icon.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   MyApp({super.key});
 
-  BoxState a = BoxState.circle;
-  BoxState b = BoxState.cross;
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isCrossTurn = true;
+
+  BoxState a = BoxState.empty;
+
+  BoxState b = BoxState.empty;
+
   BoxState c = BoxState.empty;
+
   BoxState d = BoxState.empty;
+
   BoxState e = BoxState.empty;
+
   BoxState f = BoxState.empty;
+
   BoxState g = BoxState.empty;
+
   BoxState h = BoxState.empty;
+
   BoxState i = BoxState.empty;
 
   @override
@@ -28,7 +43,10 @@ class MyApp extends StatelessWidget {
           children: [
             Expanded(
               flex: 1,
-              child: Text("Turn X", style: TextStyle(fontSize: 40)),
+              child: Text(
+                "Turn ${isCrossTurn ? "Cross" : "Circle"}",
+                style: TextStyle(fontSize: 40),
+              ),
             ),
             Expanded(
               flex: 5,
@@ -37,21 +55,177 @@ class MyApp extends StatelessWidget {
                 crossAxisSpacing: 6.0,
                 crossAxisCount: 3,
                 children: [
-                  Container(
-                    color: Colors.tealAccent,
-                    child: BoxStateIcon(boxState: a),
+                  InkWell(
+                    onTap: () {
+                      print("a taped");
+                      setState(() {
+                        if (a == BoxState.empty) {
+                          if (isCrossTurn) {
+                            a = BoxState.cross;
+                          } else {
+                            a = BoxState.circle;
+                          }
+                          isCrossTurn = !isCrossTurn;
+                        }
+                      });
+                    },
+                    child: Container(
+                      color: Colors.tealAccent,
+                      child: BoxStateIcon(boxState: a),
+                    ),
                   ),
-                  Container(
-                    color: Colors.tealAccent,
-                    child: BoxStateIcon(boxState: b),
+                  InkWell(
+                    onTap: () {
+                      print("b taped");
+                      setState(() {
+                        if (b == BoxState.empty) {
+                          if (isCrossTurn) {
+                            b = BoxState.cross;
+                          } else {
+                            b = BoxState.circle;
+                          }
+                          isCrossTurn = !isCrossTurn;
+                        }
+                      });
+                    },
+                    child: Container(
+                      color: Colors.tealAccent,
+                      child: BoxStateIcon(boxState: b),
+                    ),
                   ),
-                  Container(color: Colors.tealAccent),
-                  Container(color: Colors.tealAccent),
-                  Container(color: Colors.tealAccent),
-                  Container(color: Colors.tealAccent),
-                  Container(color: Colors.tealAccent),
-                  Container(color: Colors.tealAccent),
-                  Container(color: Colors.tealAccent),
+                  InkWell(
+                    onTap: () {
+                      print("c taped");
+                      setState(() {
+                        if (c == BoxState.empty) {
+                          if (isCrossTurn) {
+                            c = BoxState.cross;
+                          } else {
+                            c = BoxState.circle;
+                          }
+                          isCrossTurn = !isCrossTurn;
+                        }
+                      });
+                    },
+                    child: Container(
+                      color: Colors.tealAccent,
+                      child: BoxStateIcon(boxState: c),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      print("d taped");
+                      setState(() {
+                        if (d == BoxState.empty) {
+                          if (isCrossTurn) {
+                            d = BoxState.cross;
+                          } else {
+                            d = BoxState.circle;
+                          }
+                          isCrossTurn = !isCrossTurn;
+                        }
+                      });
+                    },
+                    child: Container(
+                      color: Colors.tealAccent,
+                      child: BoxStateIcon(boxState: d),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      print("e taped");
+                      setState(() {
+                        if (e == BoxState.empty) {
+                          if (isCrossTurn) {
+                            e = BoxState.cross;
+                          } else {
+                            e = BoxState.circle;
+                          }
+                          isCrossTurn = !isCrossTurn;
+                        }
+                      });
+                    },
+                    child: Container(
+                      color: Colors.tealAccent,
+                      child: BoxStateIcon(boxState: e),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      print("f taped");
+                      setState(() {
+                        if (f == BoxState.empty) {
+                          if (isCrossTurn) {
+                            f = BoxState.cross;
+                          } else {
+                            f = BoxState.circle;
+                          }
+                          isCrossTurn = !isCrossTurn;
+                        }
+                      });
+                    },
+                    child: Container(
+                      color: Colors.tealAccent,
+                      child: BoxStateIcon(boxState: f),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      print("g taped");
+                      setState(() {
+                        if (g == BoxState.empty) {
+                          if (isCrossTurn) {
+                            g = BoxState.cross;
+                          } else {
+                            g = BoxState.circle;
+                          }
+                          isCrossTurn = !isCrossTurn;
+                        }
+                      });
+                    },
+                    child: Container(
+                      color: Colors.tealAccent,
+                      child: BoxStateIcon(boxState: g),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      print("h taped");
+                      setState(() {
+                        if (h == BoxState.empty) {
+                          if (isCrossTurn) {
+                            h = BoxState.cross;
+                          } else {
+                            h = BoxState.circle;
+                          }
+                          isCrossTurn = !isCrossTurn;
+                        }
+                      });
+                    },
+                    child: Container(
+                      color: Colors.tealAccent,
+                      child: BoxStateIcon(boxState: h),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      print("i taped");
+                      setState(() {
+                        if (i == BoxState.empty) {
+                          if (isCrossTurn) {
+                            i = BoxState.cross;
+                          } else {
+                            i = BoxState.circle;
+                          }
+                          isCrossTurn = !isCrossTurn;
+                        }
+                      });
+                    },
+                    child: Container(
+                      color: Colors.tealAccent,
+                      child: BoxStateIcon(boxState: i),
+                    ),
+                  ),
                 ],
               ),
             ),
